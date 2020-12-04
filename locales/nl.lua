1fb9bcba573a774089ffcc7cfbb8844b0f783eb3
Mike | iceline.host <39988281+mike-iceline@users.noreply.github.com>
2020-12-04 14:08:09 +0100

    New file nl.lua
    Create dutch translation for ESX Personal Menu

---
+++ locales/nl.lua
@@ -0,0 +1,36 @@
+Locales['en'] = {
+	-- Main Menu
+	["main_title"]				= "Spelersmenu",
+	-- Information Menu
+	["info_title"]				= "Informatie over je karakter",
+	["identity_name"] 			= "Naam:",
+	["height"] 					= "Lengte:",
+	["birth"] 					= "Geboortedatum:",
+	["gender"]					= "Geslacht:",
+	["male"] 					= "Man",
+	["female"] 					= "Vrouw",
+	["phone"] 					= "Telefoonnummer:",
+	["wallet"] 					= "Portemonnee:",
+	["bank"] 					= "Bankrekening:",
+	["job"] 					= "Baan:",
+	["unemployed"] 				= "JE BENT WERKLOOS, ZOEK EEN BAAN!",
+	["society_account"]			= '~g~Uw bedrijf heeft een account van %s $',
+	-- Car Menu
+	["car_title"]				= "Automenu",
+	["liveries"]				= "Liveries",
+	["extras"]					= "Extras",
+	["doors"]					= "Open / Sluit alle deuren",
+	["front_doors"]				= "Voordeuren",
+	["rear_doors"]				= "Achterdeuren",
+	["hood"]					= "Motorkap",
+	["trunk"]					= "Kofferbak",
+	["engine"]					= "Motor",
+	["front_left_door"]			= "Linker voordeur",
+	["front_right_door"]		= "Rechter voordeur",
+	["rear_left_door"]			= "Linker achterdeur",
+	["rear_right_door"]			= "Rechter achterdeur",
+	-- Red Columns
+	["not_available_extras"]	= "EXTRAS MENU NIET BESCHIKBAAR",
+	["not_available_liveries"]	= "LIVERIES MENU NIET BESCHIKBAAR",
+	["not_available_car"]		= "~r~Niet beschikbaar!"
+}
